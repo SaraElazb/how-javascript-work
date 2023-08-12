82f5ac796be2128aa324e16ef37e020c0b348543
SaraElazb <alzb1518@gmail.com>
2023-08-12 17:45:52 +0300

    first commit

---
+++ Code.js
@@ -0,0 +1,26 @@
+/**
+ * How javascript work :
+ * 
+ * 1 - Engines
+ * 2 - Lexical analysis :
+ * interpreter ->
+ *        # Scanning -> parser -> tokens or chunks  (Tokenization)
+ *        # parser ->   Ignore comments and spaces
+ *        # parser ->   Error Handling
+ *        # parser ->   Token stream 
+ * 3- AST -> Abstract Syntax Tree "Creation"
+ * 4- AST Abstract Syntax Tree Traversal
+ * 5- Execution
+ * 6- Result -> Users
+ */
+
+/**
+ * Javascript Engines :
+ * 1- V8 -> chrome
+ * 2- Spider monkey -> Mozilla firefox
+ * 3- Chakra -> IE
+ * 5- JavascriptCore(Nitro) -> safari
+ * */ 
+
+//..........................................................
+
